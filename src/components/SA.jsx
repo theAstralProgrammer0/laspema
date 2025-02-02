dfdfc96dab6414e59d769743923b4b51c380ed7b
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-02-02 14:16:03 +0100

    Added environment variables

--- src/components/SA.jsx
+++ src/components/SA.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import sa from '../assets/sa.JPG'
 import { TfiLinkedin } from 'react-icons/tfi'
 import SAQuoteCard from './SAQuoteCard'
 
 
+const sa_url = `${process.env.REACT_APP_GOOGLE_DRIVE_BASE_URL}${process.env.SA_ID}`;
 const SA = () => {
   return (
     <div className="w-full bg-slate-200 py-16 px-4 ">
@@ -15,7 +15,7 @@ const SA = () => {
                 <a href='https://www.linkedin.com/in/babajide-sanwo-olu-483211172/?originalSubdomain=ng'><button className="bg-[#00df98] text-black w-[200px] rounded-md font-medium mt-7 mx-auto md:mx-0 py-3 flex justify-evenly hover:py-4 hover:w-[230px] hover:bg-[#4278f5] hover:text-slate-300 hover:text-bold ease-in-out duration-300 shadow-2xl">Connect<TfiLinkedin className="relative right-5 bottom-0.7px" size={20}/></button></a>
                 <SAQuoteCard/>
             </div>
-            <a href="https://web.facebook.com/jidesanwooluofficial/?_rdc=1&_rdr" target="blank"><img className="w-[300px] h-[300px] mx-auto my-4 rounded-full shadow-2xl hover:scale-105 ease-in-out duration-300" src={sa} alt='/' /></a>
+            <a href="https://web.facebook.com/jidesanwooluofficial/?_rdc=1&_rdr" target="blank"><img className="w-[300px] h-[300px] mx-auto my-4 rounded-full shadow-2xl hover:scale-105 ease-in-out duration-300" src={sa_url} alt='/' /></a>
         </div>
     </div>
   )
